8a1e5026cd281792168a32058bfbb544ca1273e2
mcm8 <mcm8@cdc.gov>
2023-03-10 17:09:41 -0500

    Version Bump

--- start.cmd
+++ start.cmd
@@ -1,18 +1,15 @@
 CLS
 
-SET path=C:\ProgramData\Anaconda3\envs\Streamlit;^
-C:\ProgramData\Anaconda3\envs\Streamlit\Library\mingw-w64\bin;^
-C:\ProgramData\Anaconda3\envs\Streamlit\Library\usr\bin;^
-C:\ProgramData\Anaconda3\envs\Streamlit\Library\bin;^
-C:\ProgramData\Anaconda3\envs\Streamlit\Scripts;^
-C:\ProgramData\Anaconda3\envs\Streamlit\bin;^
+SET path=C:\ProgramData\Anaconda3\envs\Streamlit; ^
+C:\ProgramData\Anaconda3\envs\Streamlit\Library\mingw-w64\bin; ^
+C:\ProgramData\Anaconda3\envs\Streamlit\Library\usr\bin; ^
+C:\ProgramData\Anaconda3\envs\Streamlit\Library\bin; ^
+C:\ProgramData\Anaconda3\envs\Streamlit\Scripts; ^
+C:\ProgramData\Anaconda3\envs\Streamlit\bin; ^
 C:\ProgramData\Anaconda3\condabin;
 
-REM %path% 
 
 CLS
-@REM CD src
-
 streamlit run .\src\dashboard.py
 
 
